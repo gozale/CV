f603cdc3f18bed9b5da0ff8e55cf18246366d9fb
gozale <a348552@uach.mx>
2023-05-09 20:29:53 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+const http=require('http');
 
+http.createServer((request,response)=>{
+    response.writeHead(200,{
+        'Content-Type': 'text/plain'
+    });
+
+    response.write("Todo bien");
+    response.end();
+}).listen(8080);
